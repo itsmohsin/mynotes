8d6e2267343e02fd621e18e3454c515c58393998
itsmohsin <mohsinsmannat@gmail.com>
2023-02-28 20:07:22 +0530

    creating a basic register screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+    );
   }
 }
